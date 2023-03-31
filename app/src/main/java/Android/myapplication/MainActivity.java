475bf055da8ca9ded2170f3e95274ae834e964e9
ikhsan intang <ikhsanlintang04@gmail.com>
2023-03-31 11:28:15 +0700

    Initial commit

---
+++ app/src/main/java/Android/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package Android.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
